3fe60a0dede3afb49d9a0b0deb7aba59693da1fa
Ryan Atkinson <mail@ryanatkn.com>
2024-07-02 07:04:44 -0600

    deployment

--- _app/immutable/nodes/4.BIOubTb5.js
+++ _app/immutable/nodes/4.BIOubTb5.js
@@ -1,3 +1,3 @@
-import{a as r,t}from"../chunks/disclose-version.Bt4mYUEc.js";var i=t(`<div class="markup svelte-18vk2t"><p>Gro previously had a dev server with an experimental frontend for visualizing and working with
+import{a as r,t}from"../chunks/disclose-version.eLoI9Jyp.js";var i=t(`<div class="markup svelte-18vk2t"><p>Gro previously had a dev server with an experimental frontend for visualizing and working with
 		build data. And then SvelteKit and Vite came along!</p> <p>It was removed in <a href="https://github.com/ryanatkn/gro/pull/321">PR #321</a> and is archived
 		here: <a href="https://github.com/spiderspace/gro/tree/archive/devserver">https://github.com/spiderspace/gro/tree/archive/devserver</a></p> <p>Vite plugins should be used going forward.</p></div>`);function o(e){var a=i();r(e,a)}export{o as component};
